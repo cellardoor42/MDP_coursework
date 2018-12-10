007af7e52c4f76e67ff6a30e79dd9b21c2c7d682
Sophya Borushko <sophya.b42@gmail.com>
2018-12-10 21:54:06 +0300

    initial commit

---
+++ app/src/main/java/com/example/movierate/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.movierate
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.support.design.widget.BottomNavigationView.*
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    private val mOnNavigationItemSelectedListener = OnNavigationItemSelectedListener { item ->
+        when (item.itemId) {
+            R.id.navigation_home -> {
+                val homeFragment = HomeFragment.newInstance()
+                openFragment(homeFragment)
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_ratings -> {
+                val ratingsFragment = RatingsFragment.newInstance()
+                openFragment(ratingsFragment)
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+        false
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val homeFragment = HomeFragment.newInstance()
+        openFragment(homeFragment)
+
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+    }
+
+    private fun openFragment(fragment: Fragment) {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.container, fragment)
+        transaction.addToBackStack(null)
+        transaction.commit()
+    }
+}
